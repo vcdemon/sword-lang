52c9aabaf342942222fadafe96be3dad46b10986
vcdemon <345280563@qq.com>
2016-05-30 15:31:56 +0800

    Signed-off-by: vcdemon <345280563@qq.com>

--- src/org/sword/lang/JaxbParser.java
+++ src/org/sword/lang/JaxbParser.java
@@ -14,7 +14,6 @@ import javax.xml.bind.Unmarshaller;
 
 import org.apache.log4j.Logger;
 
-
 import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
